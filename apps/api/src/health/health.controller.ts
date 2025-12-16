fe73f393a5719d1c124175dce0ad4564c08c4722
ELaidet <erik.laidet@gmail.com>
2025-12-16 16:40:08 +0100

    adjusting ci.yml

--- apps/api/src/health/health.controller.ts
+++ apps/api/src/health/health.controller.ts
@@ -3,6 +3,11 @@ import { Controller, Get } from '@nestjs/common';
 @Controller('health')
 export class HealthController {
   @Get()
+  @Get('/health')
+	healthCheck() {
+	  return { status: 'ok', timestamp: new Date().toISOString() };
+	}
+
   check() {
     return {
       status: 'ok',
